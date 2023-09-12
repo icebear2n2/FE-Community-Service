f4b7755d201884fbf10cd692fed794ddafae93cb
icebear2n2 <icebear2n2@gmail.com>
2023-09-12 19:19:23 +0900

    FEAT: Template 구현 완료

--- src/templates/Template.jsx
+++ src/templates/Template.jsx
@@ -1,15 +1,17 @@
 import MyHeader from "../components/MyHeader";
-import {Outlet} from "react-router";
+import { Outlet } from "react-router";
 import MyFooter from "../components/MyFooter";
+import Menubar from "../components/Menubar";
 
 const Template = () => {
-    return (
-        <>
-            <MyHeader />
-            <Outlet />
-            <MyFooter />
-        </>
-    )
-}
+  return (
+    <>
+      <MyHeader />
+      <Menubar />
+      <Outlet />
+      <MyFooter />
+    </>
+  );
+};
 
 export default Template;
